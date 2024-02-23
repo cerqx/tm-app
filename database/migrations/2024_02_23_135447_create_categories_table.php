e801750c69e2602837948ff28f412f615abcc140
Cerqueira <contatocerq@gmail.com>
2024-02-23 10:57:55 -0300

    Done

--- database/migrations/2024_02_23_135447_create_categories_table.php
+++ database/migrations/2024_02_23_135447_create_categories_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->longText('description');
-            $table->decimal('price');
-            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
